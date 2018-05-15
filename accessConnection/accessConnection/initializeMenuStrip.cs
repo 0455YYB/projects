d813b1cfda77cd9f124c83b7a7521a3ccca558e1
0455YYB <tugueiyin@163.com>
2018-05-15 21:31:22 +0800

    2018-05-15
    menu Strip 添加菜单测试

--- accessConnection/accessConnection/initializeMenuStrip.cs
+++ accessConnection/accessConnection/initializeMenuStrip.cs
@@ -11,7 +11,15 @@ namespace accessConnection
         MenuStrip MenuStrip;
         public static void createMenuStrip(MenuStrip ms)
         {
-            
+            ToolStripMenuItem topMenu = new ToolStripMenuItem();
+            topMenu.Name = "top菜单1";
+            topMenu.Text = "top菜单1";
+            ms.Items.Add(topMenu);
+
+            ToolStripDropDownMenu seccondMenu = new ToolStripDropDownMenu();
+            seccondMenu.Name = "seccond菜单1";
+            seccondMenu.Text = "seccond菜单1";
+            seccondMenu.Items.Add
         }
 
         private void menu_click(object senders,EventArgs e)
