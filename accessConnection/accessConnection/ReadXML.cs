985a01aea09aaa742f368141a44f0f683e5e839f
0455YYB <tugueiyin@163.com>
2018-05-22 18:05:44 +0800

    2018-05-22
    read xml

---
+++ accessConnection/accessConnection/ReadXML.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Windows.Forms;
+
+namespace accessConnection
+{
+    class ReadXML
+    {
+        public string dataBaseName;
+        public string personName;
+        public string age;
+        public string email;
+        public void getXML()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(System.Environment.CurrentDirectory+"\\config.xml");
+            XmlElement xmlElement = xmlDocument.DocumentElement;
+            XmlNodeList xmlNodeList = xmlElement.GetElementsByTagName("person");
+            personName = xmlElement.GetAttribute("name");
+            age = xmlElement.GetAttribute("age");
+            email = xmlElement.GetAttribute("email");
+            Console.WriteLine("姓名"+personName+"年龄"+age+"邮箱"+email);
+           
+        }
+    }
+}
