51c04c63390161addd79e803ea4e64c2ca9aa1ab
0455YYB <tugueiyin@163.com>
2018-06-04 09:01:10 +0800

    2018-06-01
    crate xml node

--- RadioPlay/RadioPlay/OprXML.cs
+++ RadioPlay/RadioPlay/OprXML.cs
@@ -10,10 +10,42 @@ namespace RadioPlay
     {
         public void CreateXML(string fileName)
         {
-            XmlDocument xmlDocument = new XmlDocument();
-            xmlDocument.Load(fileName);
-            var root = xmlDocument.DocumentElement;
+            try
+            {
+                bool file = System.IO.File.Exists(System.Environment.CurrentDirectory + "\\config1.xml");
 
+                if (file==false)
+                {
+                    XmlDocument xmlDocument = new XmlDocument();
+                    XmlDeclaration xml = xmlDocument.CreateXmlDeclaration("1.0", "utf-8", string.Empty);
+                    xmlDocument.AppendChild(xml);
+                    XmlElement rootNoe = xmlDocument.CreateElement("dllCofig");
+                    xmlDocument.AppendChild(rootNoe);
+                    XmlElement dllName = xmlDocument.CreateElement("dllName");
+                    dllName.InnerText = "radioPlay.dll";
+                    rootNoe.AppendChild(dllName);
+                    xmlDocument.Save("config1.xml");
+                    System.Windows.Forms.MessageBox.Show("config1.xml创建成功");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("文件已经存在");
+                    return;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+            //xmlDocument.Load(fileName);
+            //var root = xmlDocument.DocumentElement;
+            //XmlElement personXML = xmlDocument.CreateElement("person");
+            //personXML.InnerText = "大家";
+            //personXML.SetAttribute("", "");
+            //root.AppendChild(personXML);
+
+            //XmlNodeList sd = xmlDocument.GetElementsByTagName("");
         }
 
         public void CreateXmlNode(XmlNode xn)
@@ -30,5 +62,17 @@ namespace RadioPlay
         {
 
         }
+
+        public void SelectXmlNode()
+        {
+            string fileName = System.Environment.CurrentDirectory + "\\config.xml";
+            XmlDataDocument config = new XmlDataDocument();
+            config.Load(fileName);
+
+
+
+        }
+            
+                               
     }
 }
