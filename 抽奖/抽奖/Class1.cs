a4056ab1f5e29e6cef7cdb4f19f4a468dbc56bee
0455YYB <tugueiyin@163.com>
2019-03-15 19:48:11 +0800

    2019-03-15

---
+++ 抽奖/抽奖/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 抽奖
+{
+    class Class1
+    {
+        //存放幸运号码
+        public static int number_size=42;
+        public int[] number = new int[number_size];
+
+        //对应相片
+        public string[] pic_name = new string[number_size];
+    }
+}
---
+++ 抽奖/抽奖/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 抽奖
+{
+    public partial class Form1 : Form
+    {
+        public int start = 1;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text==null)
+            {
+                MessageBox.Show("请输入最大人数");
+            }
+            else
+            {
+                if (start == 1)
+                {
+                    button1.Text = "停 止";
+                    start = 0;
+                    timer1.Start();
+                }
+                else
+                {
+                    button1.Text = "开 始";
+                    start = 1;
+                    timer1.Stop();
+
+                }
+            }
+            
+
+        
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int maxNumber = Convert.ToInt32(textBox2.Text);
+            Random rd = new Random();
+            int lukyNumber = rd.Next(1, maxNumber);
+            textBox1.Text = lukyNumber.ToString();
+          
+        }
+    }
+}
